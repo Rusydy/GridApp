a08896b2b08c8c0d6e64574d21382d639cce9c05
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-09-08 16:51:50 +0700

    Implement Grid App

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -1,51 +1,120 @@
 package com.example.gridapp
 
 import android.os.Bundle
+import android.os.ext.SdkExtensions
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.gridapp.data.DataSource
 import com.example.gridapp.model.Topic
 import com.example.gridapp.ui.theme.GridAppTheme
 
+@RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             GridAppTheme {
-                // TODO: Implement the GridApp
+                GridApp()
             }
         }
     }
 }
 
+@RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
 @Composable
 fun GridApp() {
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
+        val topics = DataSource().loadTopics()
+        GridCardList(topics = topics)
+    }
+}
 
+@RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GridAppPreview() {
+    GridAppTheme {
+        GridApp()
+    }
+}
+
+@Composable
+fun GridCardList(topics: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2), modifier = modifier
+    ) {
+        items(topics) { topic ->
+            TopicCard(topic = topic)
+        }
+    }
+}
+
+@RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GridCardListPreview() {
+    val topics = DataSource().loadTopics()
+    GridAppTheme {
+        GridCardList(topics = topics)
+    }
 }
 
 @Composable
 fun TopicCard(topic: Topic) {
-    Card {
+    Card(
+        modifier = Modifier.padding(
+            start = dimensionResource(id = R.dimen.padding_small),
+            end = dimensionResource(id = R.dimen.padding_small),
+            bottom = dimensionResource(id = R.dimen.padding_small)
+        )
+    ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
@@ -94,7 +163,7 @@ fun TopicCard(topic: Topic) {
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GridAppCardPreview() {
+fun TopicCardPreview() {
     GridAppTheme {
         TopicCard(
             topic = Topic(
@@ -103,13 +172,5 @@ fun GridAppCardPreview() {
                 imageRes = R.drawable.architecture
             )
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GridAppTheme {
-        GridApp()
     }
 }
